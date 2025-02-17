010cdb3528d5a69f3c3b55971fe97720eae0dc10
jaseeeeeeel <9809001229jaseel@gmail.com>
2025-02-17 08:24:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,5 +3,8 @@ provider "aws" {
 resource "aws_instance" "web" {
   count = 1
   ami = "ami-123"
-  instance_type = "t2.micrp"
+  instance_type = "t2.micro"
+  tag = {
+    Name = "testserver"
+  }
 }
